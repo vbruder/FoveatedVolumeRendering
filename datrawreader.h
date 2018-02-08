0e3b9cfce994a104a11a138102a7cfe63ffa3a1f
Valentin Bruder <valentin.bruder@visus.uni-stuttgart.de>
2018-02-08 17:34:15 +0100

    added copyright headers to all files

--- datrawreader.h
+++ datrawreader.h
@@ -1,7 +1,24 @@
-/// <copyright file="util.h" company="SFB-TRR 161 Quantitative Methods for Visual Computing">
-/// Copyright ? 2017 SFB-TRR 161. Alle Rechte vorbehalten.
-/// </copyright>
-/// <author>Valentin Bruder</author>
+/**
+ * \file
+ *
+ * \author Valentin Bruder
+ *
+ * \copyright Copyright (C) 2018 Valentin Bruder
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
 #pragma once
 
